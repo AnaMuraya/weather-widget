50b02205bce3f6ead923aec0a7db2bedb40d0edf
Ana Muraya <Anamuraya9@gmail.com>
2022-07-08 20:57:08 +0300

    Added the latitude and longitude states

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import './App.scss';
+import { useEffect, useState } from "react";
+import "./App.scss";
 
 function App() {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+
+    console.log("Latitude is: ", lat);
+    console.log("Longitude is: ", long);
+  }, [lat, long]);
+
   return (
-    <div className="App">
+    <div className="App" data-testid="app">
       Weather!!!
     </div>
   );
